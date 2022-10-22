32cac3b02075c135a813ea2d6563d68bb2d10fba
Gocer615 <gtumayy21@gmail.com>
2022-10-22 13:39:58 +0300

    feat: making enemies

---
+++ Assets/Scripts/EnemyCreated.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreated : MonoBehaviour
+{
+    int time;
+    public GameObject enemy;
+    void Start()
+    {
+        time = 0;
+    }
+
+    
+    void Update()
+    {
+        if (time >= 2)
+        {
+            Instantiate(enemy, new Vector3(0f, 0.6f, 11f), Quaternion.identity);
+            time = 0;
+        }
+        else
+        {
+            time += 1;
+        }
+    }
+}
