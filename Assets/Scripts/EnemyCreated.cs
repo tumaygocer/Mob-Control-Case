3150c587547546d6c3935a2d3098f37338c94bdf
Gocer615 <gtumayy21@gmail.com>
2022-10-23 14:13:09 +0300

    feat: enemyCount added

--- Assets/Scripts/EnemyCreated.cs
+++ Assets/Scripts/EnemyCreated.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EnemyCreated : MonoBehaviour
 {
     int time;
     public GameObject enemy;
     int enemymake = 50;
+    public TextMeshProUGUI enemyCount;
     void Start()
     {
         time = 0;
@@ -15,10 +17,11 @@ public class EnemyCreated : MonoBehaviour
     
     void Update()
     {
-        if (time >= 60 && enemymake != 0)
+        if (time >= 10 && enemymake != 0)
         {
             Instantiate(enemy, new Vector3(0f, 0.6f, 11f), Quaternion.identity);
             enemymake -= 1;
+            enemyCount.text = enemymake.ToString();
             time = 0;
         }
         else
