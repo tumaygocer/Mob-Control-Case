49fe19e8ed014cfbb742e6dd12a2e4c8e03a2241
Gocer615 <gtumayy21@gmail.com>
2022-10-24 09:14:58 +0300

    feat: clone creation added

--- Assets/Scripts/GeneralScript.cs
+++ Assets/Scripts/GeneralScript.cs
@@ -27,4 +27,6 @@ public class GeneralScript : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+   
 }
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    public SphereCollider sphere;
+    public GameObject self;
+
     void Update()
     {
         transform.Translate(Vector3.forward);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("clonner"))
+        {
+            self.GetComponentInChildren<MeshRenderer>().enabled = true;
+            sphere.enabled = true;
+        }
     }
 }
